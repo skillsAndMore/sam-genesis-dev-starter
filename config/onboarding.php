7985deffb5a6ee4b4494369660d3b3a8d015a7cc
Andrea Barghigiani <a.barghigiani@gmail.com>
2019-12-16 18:16:07 +0100

    create basic onboarding.php

---
+++ config/onboarding.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * SkillsAndMore Genesis Dev Starter.
+ *
+ * Onboarding config to load plugins and homepage content on theme activation.
+ *
+ * @package SkillsAndMore Genesis Dev Starter
+ * @author  SkillsAndMore
+ * @license GPL-2.0-or-later
+ * @link    https://skillsandmore.org/
+ */
+
+return [
+	'dependencies'     => [
+		'plugins' => [],
+	],
+	'content'          => [],
+	'navigation_menus' => [],
+	'widgets'          => [],
+];
